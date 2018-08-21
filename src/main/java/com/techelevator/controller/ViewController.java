b7e8325453f14b9d57431209e7dd2802061ba750
Ryan Palaschak <rjpst19@me.com>
2018-08-21 12:22:32 -0400

    homepage added, working on login & register

---
+++ src/main/java/com/techelevator/controller/ViewController.java
@@ -0,0 +1,25 @@
+package com.techelevator.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.techelevator.model.UserDAO;
+
+@Controller
+public class ViewController {
+	
+	
+	@RequestMapping (path="/", method=RequestMethod.GET)
+	public String displayHomepage() {
+		return "homepage";
+	}
+	
+	
+	
+}
