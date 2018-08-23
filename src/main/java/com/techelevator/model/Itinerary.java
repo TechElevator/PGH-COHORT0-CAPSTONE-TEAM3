6537f227b7bb0a87c7874623c6209f9de4fe0fc8
Andrew Rich <andrewrich@gmail.com>
2018-08-23 11:32:16 -0400

    added itinerary object

---
+++ src/main/java/com/techelevator/model/Itinerary.java
@@ -0,0 +1,67 @@
+package com.techelevator.model;
+
+import java.util.Date;
+
+public class Itinerary {
+	
+	private String place_id;
+	private String name;
+	private double lat;
+	private double lng;
+	private Date date;
+	private String startingPoint;  //this could be the google place id, 
+									//not sure if we need lng/lat for this yet
+	
+	public Itinerary() {
+		
+	}
+
+	public String getPlace_id() {
+		return place_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getLat() {
+		return lat;
+	}
+
+	public double getLng() {
+		return lng;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+	
+	public String getStartingPoint() {
+		return startingPoint;
+	}
+
+	public void setPlace_id(String place_id) {
+		this.place_id = place_id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+
+	public void setLng(double lng) {
+		this.lng = lng;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public void setStartingPoint(String startingPoint) {
+		this.startingPoint = startingPoint;
+	}
+	
+}
